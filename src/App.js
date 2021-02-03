2f77d8019e72d55ebf837152283cde488bc83f38
lucky <takuto.yoshikai@gmail.com>
2021-02-03 19:52:33 +0900

    tab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import "./form.css";
 import ReactDOM from "react-dom";
 import * as components from "./components";
 import * as eos from "./eos";
 
 async function showCertificates() {
-  const receiver = document.getElementById("receiver").value;
+  const receiver = document.getElementById("holder").value;
   const certificates = await eos.getMyCertificates(receiver);
   ReactDOM.render(<components.CertificateComponents certificates={certificates} />, document.getElementById("result"));
+}
+
+function resetTabSelected() {
+  document.getElementById("issue-tab").classList.remove("selected");
+  document.getElementById("show-tab").classList.remove("selected");
+  document.getElementById("issue").classList.remove("hidden");
+  document.getElementById("show").classList.remove("hidden");
+}
+function changeTabToIssue() {
+  resetTabSelected();
+  document.getElementById("issue-tab").classList.add("selected");
+  document.getElementById("show").classList.add("hidden");
+}
+
+function changeTabToShow() {
+  resetTabSelected();
+  document.getElementById("show-tab").classList.add("selected");
+  document.getElementById("issue").classList.add("hidden");
 }
 
 function App() {
   return (
     <div className="App">
-      <div className="Main">
-        issueser: <input type="text" id="issueser"/><br/>
-        receiver: <input type="text" id="receiver"/><br/>
-        cert-image: <input type="file" id="cert-image" onChange={ eos._createCertificate } /><br />
-        <input type="button" onClick={ showCertificates }/><br/>
-      </div>
-      <div id="result">
+      <div className="main">
+        <div className="tabs">
+          <div className="issue-tab tab selected" id="issue-tab" onClick={ changeTabToIssue }>
+            Issue
+          </div>
+          <div className="show-tab tab" id="show-tab" onClick= { changeTabToShow }>
+            Show
+          </div>
+        </div><br/>
+        <div id="show" className="show" class="form-group hidden">
+          <h2>Show Certificates</h2>
+          <label>Certificate Holder</label>
+          <input type="text" id="holder" class="form-control" placeholder="alice" />
+          <input type="button" value="Show certificates" onClick={ showCertificates } id="show-cert" class="form-control" /><br/>
+          <div id="result">
+          </div>
+        </div>
+        <div className="issue" class="form-group" id="issue">
+          <h2>Issue Certificate</h2>
+          <label for="issueser">issueser</label>
+          <input type="text" id="issueser" class="form-control" placeholder="alice" />
+          <label for="receiver">receiver</label> 
+          <input type="text" id="receiver" class="form-control" placeholder="bob" />
+          <label>Certificate Image</label>
+          <input type="file" id="cert-image" onChange={ eos._createCertificate } class="form-control" /><br />
+        </div>
       </div>
     </div>
   )
