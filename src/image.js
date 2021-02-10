2315b57de72cd33a93f5ac7972f49a0667e2d252
lucky <takuto.yoshikai@gmail.com>
2021-02-10 19:35:10 +0900

    make hash from image

--- src/image.js
+++ src/image.js
@@ -1,3 +1,43 @@
+const crypto = require("crypto");
+
+function getPixelsFromContext(context, width, height) {
+  let result = [];
+  for (let i = 0; i < height; i++) {
+    result.push([]);
+    for (let j = 0; j < width; j++) {
+      const pixel = context.getImageData(j, i, 1, 1);
+      result[i].push([pixel[0], pixel[1], pixel[2]]);
+    }
+  }
+  return result;
+}
+function getPixelsFromImage(fileElement) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    const img = new Image();
+    reader.onload = function() {
+      img.src = reader.result;
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      context.drawImage(img, 0, 0);
+      const pixels = getPixelsFromContext(context, img.width, img.height);
+      resolve(pixels);
+    }
+    reader.readAsDataURL(fileElement.files[0]);
+  });
+}
+
+function makeHash(pixels) {
+  const text = pixels.map(row => {
+    return row.map(pixel => {
+      return pixel.toString();
+    }).join("");
+  }).join("");
+  console.log(text);
+  const md5 = crypto.createHash("md5");
+  return md5.update(text, "binary").digest("hex");
+}
+
 function fileInputToDataURL(element) {
   return new Promise((resolve, reject) => {
     const file = element.files[0];
