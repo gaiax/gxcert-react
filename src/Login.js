b1db72f75d2653e30af71490cc9dc7f7adbe22cd
lucky <takuto.yoshikai@gmail.com>
2021-03-03 16:07:07 +0900

    login.css

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "./Login.css";
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onClick: props.onClick,
+    }
+  }
   render() {
     return (
       <div className="login-page">
-        
+        <button className="login-google" onClick={this.state.onClick}>
+          Login Google
+        </button>
       </div>
     );
   }
