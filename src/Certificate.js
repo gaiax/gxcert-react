c87e48c7923191fa199a3358f4a58b74b33bf423
lucky <takuto.yoshikai@gmail.com>
2021-03-20 17:09:59 +0900

    next back

--- src/Certificate.js
+++ src/Certificate.js
@@ -77,35 +77,53 @@ class CertViewComponent extends React.Component {
     const index = parseInt(props.match.params.index);
     this.index = index;
     this.state = {
-      certificate: props.certificates[index],
+      certificates: props.certificates,
+      index: index,
     }
   }
-  componentWillMount() {
-    const that = this;
-    (async () => {
-      console.log(that.state.certificate);
-      const imageUrl = await ipfs.getCertificateImage(that.state.certificate.ipfs);
-      console.log(imageUrl);
-      that.setState({
-        imageUrl
+  back() {
+    if (this.state.index > 0) {
+      this.setState({
+        index: this.state.index - 1,
       });
-    })();
+      this.loadImage();
+    }
+  }
+  next() {
+    if (this.state.index < this.state.certificates.length - 1) {
+      this.setState({
+        index: this.state.index + 1,
+      });
+      this.loadImage();
+    }
+  }
+  async loadImage() {
+    const certificate = this.state.certificates[this.state.index];
+    const imageUrl = await ipfs.getCertificateImage(certificate.ipfs);
+    console.log(imageUrl);
+    this.setState({
+      imageUrl
+    });
+  }
+  componentWillMount() {
+    this.loadImage();
   }
   render() {
+    const certificate = this.state.certificates[this.state.index];
     return (
       <div className="cert-view">
         <div className="cert-view-top">
-          <button className="back">＜</button>
+          <button className="back" onClick={this.back.bind(this)}>＜</button>
           <img src={this.state.imageUrl} className="cert-view-image" alt="GxCert" />
-          <button className="next">＞</button>
+          <button className="next" onClick={this.next.bind(this)}>＞</button>
         </div>
         <div className="cert-view-bottom">
           <p className="cert-view-issueser">
-            { !this.state.certificate.issueserName ? this.state.certificate.by.substr(0, 16) : this.state.certificate.issueserName }
+            { !certificate.issueserName ? certificate.by.substr(0, 16) : certificate.issueserName }
           </p>
           <p className="cert-view-date">
             {
-              dateString(new Date(this.state.certificate.time * 1000))
+              dateString(new Date(certificate.time * 1000))
             }
           </p>
         </div>
