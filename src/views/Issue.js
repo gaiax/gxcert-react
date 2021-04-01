89217698424b0b4c730f3de63cc1af6fd34fccb7
lucky <takuto.yoshikai@gmail.com>
2021-04-01 19:37:46 +0900

    change styles

--- src/views/Issue.js
+++ src/views/Issue.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Issue.css";
+import "./reset.css";
 
 
 class IssueComponent extends React.Component {
@@ -11,7 +12,7 @@ class IssueComponent extends React.Component {
           <h5 className="issue-image-title issue-input-title">Certificate Image</h5>
           <input type="file" id="issue-image" className="issue-image issue-input" onChange={this.props.onChangeCertificateImage} />
           <h5 className="issue-to-title issue-input-title">Title</h5>
-          <input type="text" id="issue-title" className="issue-title issue-input" onChange={this.props.onChangeTitle} /><br/>
+          <input type="text" id="issue-title" className="issue-input" onChange={this.props.onChangeTitle} /><br/>
           <h5 className="issue-to-title issue-input-title">To</h5>
           <input type="text" id="issue-to" className="issue-to issue-input" onChange={this.props.onChangeIssueTo} /><br/>
           <button className="issue-button" id="issue-button" onClick={this.props.onClickIssueButton}>Issue</button>
