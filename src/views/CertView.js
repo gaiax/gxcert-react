404e509960a3e7f09ca3d0daf662b667a8519aec
lucky <takuto.yoshikai@gmail.com>
2021-04-14 20:16:00 +0900

    verified bug

--- src/views/CertView.js
+++ src/views/CertView.js
@@ -82,7 +82,8 @@ class CertViewComponent extends React.Component {
         <div className="cert-view-bottom">
           <button onClick={this.verifyCertificate.bind(this)} className="verify-button">Verify</button>
           <p className="cert-view-title">
-            { this.state.verified ? "✅ This certificate is valid." : "❌ This certificate is invalid." }<br/>
+            { this.state.verified ? "✅ This certificate is valid." : "" }
+            { !this.state.verified && this.state.verified !== null ? "❌ This certificate is invalid." : "" }<br/>
             { this.state.titleInIpfs } by <Link to={"/users/" + certificate.by }>{ !certificate.issueserName ? certificate.by.substr(0, 16) : certificate.issueserName }</Link> { !certificate.to ? "" : "to " + certificate.to.substr(0, 16) } at {
               dateString(new Date(certificate.time * 1000))
             }
