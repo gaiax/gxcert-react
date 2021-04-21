8d120670470aa40e7d1e51009cd3425cb305c1ca
lucky <takuto.yoshikai@gmail.com>
2021-04-21 19:25:57 +0900

    client with ipfs

---
+++ src/util/ClientWithIpfs.js
@@ -0,0 +1,68 @@
+import { getTextOnIpfs, getImageOnIpfs } from "../image-upload";
+async function getCertificates(client, address, update) {
+  const certificates = await client.getCertificates(address);
+  update(certificates);
+  for (const certificate of certificates) {
+    getImageOnIpfs(certificate.ipfs).then(imageUrl => {
+      certificate.imageUrl = imageUrl;
+      update(certificates);
+    }).catch(err => {
+      console.error(err);
+    });
+    getTextOnIpfs(certificate.title).then(title => {
+      certificate.titleInIpfs = title;
+      update(certificates);
+    }).catch(err => {
+      console.error(err);
+    });
+    client.getProfile(certificate.by).then(byProfile => {
+      certificate.byProfile = byProfile;
+    }).catch(err => {
+      console.error(err);
+    });
+    if (certificate.to) {
+      client.getProfile(certificate.to).then(toProfile => {
+        certificate.toProfile = toProfile;
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+  }
+}
+async function getCertificatesIIssuesed(client, address, update) {
+  const certificates = await client.getCertificatesIIssuesed(address);
+  update(certificates);
+  for (const certificate of certificates) {
+    getImageOnIpfs(certificate.ipfs).then(imageUrl => {
+      certificate.imageUrl = imageUrl;
+      update(certificates);
+    }).catch(err => {
+      console.error(err);
+    });
+    getTextOnIpfs(certificate.title).then(title => {
+      certificate.titleInIpfs = title;
+      update(certificates);
+    }).catch(err => {
+      console.error(err);
+    });
+    client.getProfile(certificate.by).then(byProfile => {
+      certificate.byProfile = byProfile;
+      update(certificates);
+    }).catch(err => {
+      console.error(err);
+    });
+    if (certificate.to) {
+      client.getProfile(certificate.to).then(toProfile => {
+        certificate.toProfile = toProfile;
+        update(certificates);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+  }
+}
+
+export {
+  getCertificates,
+  getCertificatesIIssuesed,
+}
