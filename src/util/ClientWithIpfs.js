8a376df52ea833cfdbef77baf020dce189e3df5b
lucky <takuto.yoshikai@gmail.com>
2021-04-21 21:47:38 +0900

    null check

--- src/util/ClientWithIpfs.js
+++ src/util/ClientWithIpfs.js
@@ -9,24 +9,30 @@ async function getCertificates(client, address, update, fail) {
   }
   update(certificates);
   for (const certificate of certificates) {
-    getImageOnIpfs(certificate.ipfs).then(imageUrl => {
-      certificate.imageUrl = imageUrl;
-      update(certificates);
-    }).catch(err => {
-      console.error(err);
-    });
-    getTextOnIpfs(certificate.title).then(title => {
-      certificate.titleInIpfs = title;
-      update(certificates);
-    }).catch(err => {
-      console.error(err);
-    });
-    client.getProfile(certificate.by).then(byProfile => {
-      certificate.byProfile = byProfile;
-    }).catch(err => {
-      console.error(err);
-    });
-    if (certificate.to) {
+    if (!certificate.imageUrl) {
+      getImageOnIpfs(certificate.ipfs).then(imageUrl => {
+        certificate.imageUrl = imageUrl;
+        update(certificates);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+    if (!certificate.titleInIpfs) {
+      getTextOnIpfs(certificate.title).then(title => {
+        certificate.titleInIpfs = title;
+        update(certificates);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+    if (!certificate.byProfile) {
+      client.getProfile(certificate.by).then(byProfile => {
+        certificate.byProfile = byProfile;
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+    if (!certificate.toProfile && certificate.to) {
       client.getProfile(certificate.to).then(toProfile => {
         certificate.toProfile = toProfile;
       }).catch(err => {
@@ -45,28 +51,32 @@ async function getCertificatesIIssuesed(client, address, update, fail) {
   }
   update(certificates);
   for (const certificate of certificates) {
-    getImageOnIpfs(certificate.ipfs).then(imageUrl => {
-      certificate.imageUrl = imageUrl;
-      update(certificates);
-    }).catch(err => {
-      console.error(err);
-    });
-    getTextOnIpfs(certificate.title).then(title => {
-      certificate.titleInIpfs = title;
-      update(certificates);
-    }).catch(err => {
-      console.error(err);
-    });
-    client.getProfile(certificate.by).then(byProfile => {
-      certificate.byProfile = byProfile;
-      update(certificates);
-    }).catch(err => {
-      console.error(err);
-    });
-    if (certificate.to) {
+    if (!certificate.imageUrl) {
+      getImageOnIpfs(certificate.ipfs).then(imageUrl => {
+        certificate.imageUrl = imageUrl;
+        update(certificates);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+    if (!certificate.titleInIpfs) {
+      getTextOnIpfs(certificate.title).then(title => {
+        certificate.titleInIpfs = title;
+        update(certificates);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+    if (!certificate.byProfile) {
+      client.getProfile(certificate.by).then(byProfile => {
+        certificate.byProfile = byProfile;
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+    if (!certificate.toProfile && certificate.to) {
       client.getProfile(certificate.to).then(toProfile => {
         certificate.toProfile = toProfile;
-        update(certificates);
       }).catch(err => {
         console.error(err);
       });
