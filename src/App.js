ff5f4c2e146a06cc4aaec2d08745b0c1f38efb63
lucky <takuto.yoshikai@gmail.com>
2021-04-27 20:23:25 +0900

    fix navbar name bug

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends React.Component {
           <header>
           <h2 className="brand-logo"><a href="/">GxCert</a></h2>
           <Link to="/issue" className="header-issue-button header-button">Issue</Link>
-          <Link to="/" className="header-show-button header-button">{client !== null && client.profile && client.profile.name ? client.profile.name.substr(0, 6) + "..." : "Profile" }</Link>
+          <Link to="/" className="header-show-button header-button">{client !== null && client.profile && client.profile.nameInIpfs ? client.profile.nameInIpfs.substr(0, 6) + "..." : "Profile" }</Link>
           </header>
           <div className="main">
             <Switch>
